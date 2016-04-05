073fa09b511d1bfe8befd61b2d2dc93ab33d8d7c
Roberts Guļāns <roberts@rego.lv>
2016-04-05 10:04:35 +0300

    Added dialyzer to protocols

--- lib/ravel/blank.ex
+++ lib/ravel/blank.ex
@@ -47,42 +47,70 @@ defprotocol Ravel.Blank do
     false
   """
 
+  @spec blank?(any) :: boolean
   def blank?(value)
 end
 
 defimpl Ravel.Blank, for: List do
+  @spec blank?(list) :: boolean
   def blank?([]), do: true
   def blank?(_),  do: false
 end
 
 defimpl Ravel.Blank, for: Float do
+  @spec blank?(float) :: boolean
   def blank?(0.0), do: true
   def blank?(_), do: false
 end
 
 defimpl Ravel.Blank, for: Integer do
+  @spec blank?(integer) :: boolean
   def blank?(0), do: true
   def blank?(_), do: false
 end
 
 defimpl Ravel.Blank, for: Tuple do
+  @spec blank?(tuple) :: boolean
   def blank?({}), do: true
   def blank?(_),  do: false
 end
 
 defimpl Ravel.Blank, for: BitString do
+  @spec blank?(binary) :: boolean
   def blank?(""), do: true
   def blank?(_),  do: false
 end
 
 defimpl Ravel.Blank, for: Atom do
+  @spec blank?(atom) :: boolean
   def blank?(nil),   do: true
   def blank?(false), do: true
   def blank?(_),     do: false
 end
 
 defimpl Ravel.Blank, for: Map do
+  @spec blank?(map) :: boolean
   def blank?(map), do: map_size(map) == 0
+end
+
+defimpl Ravel.Blank, for: Function do
+  @spec blank?(fun) :: boolean
+  def blank?(function), do: :erlang.fun_info(function)[:arity] == 0
+end
+
+defimpl Ravel.Blank, for: PID do
+  @spec blank?(pid) :: boolean
+  def blank?(_pid), do: false
+end
+
+defimpl Ravel.Blank, for: Port do
+  @spec blank?(port) :: boolean
+  def blank?(_port), do: false
+end
+
+defimpl Ravel.Blank, for: Reference do
+  @spec blank?(reference) :: boolean
+  def blank?(_reference), do: false
 end
 
 defimpl Ravel.Blank, for: Any do
--- lib/ravel/size.ex
+++ lib/ravel/size.ex
@@ -38,35 +38,63 @@ defprotocol Ravel.Size do
     2
   """
 
+  @spec size(any) :: non_neg_integer
   def size(value)
 end
 
 defimpl Ravel.Size, for: List do
+  @spec size(list) :: non_neg_integer
   def size(x), do: Enum.count x
 end
 
 defimpl Ravel.Size, for: Float do
+  @spec size(float) :: non_neg_integer
   def size(x), do: x
 end
 
 defimpl Ravel.Size, for: Integer do
+  @spec size(integer) :: non_neg_integer
   def size(x), do: x
 end
 
 defimpl Ravel.Size, for: Tuple do
+  @spec size(tuple) :: non_neg_integer
   def size(x), do: tuple_size x
 end
 
 defimpl Ravel.Size, for: BitString do
+  @spec size(binary) :: non_neg_integer
   def size(x), do: String.length x
 end
 
 defimpl Ravel.Size, for: Atom do
+  @spec size(atom) :: non_neg_integer
   def size(nil),   do: 0
   def size(false), do: 0
   def size(x),     do: String.length Atom.to_string x
 end
 
 defimpl Ravel.Size, for: Map do
+  @spec size(map) :: non_neg_integer
   def size(map), do: map_size(map)
+end
+
+defimpl Ravel.Size, for: Function do
+  @spec size(fun) :: non_neg_integer
+  def size(function), do: :erlang.fun_info(function)[:arity]
+end
+
+defimpl Ravel.Size, for: PID do
+  @spec size(pid) :: non_neg_integer
+  def size(pid), do: 1 # What?
+end
+
+defimpl Ravel.Size, for: Port do
+  @spec size(port) :: non_neg_integer
+  def size(port), do: 1 # What?
+end
+
+defimpl Ravel.Size, for: Reference do
+  @spec size(reference) :: non_neg_integer
+  def size(reference), do: 1 # What?
 end
